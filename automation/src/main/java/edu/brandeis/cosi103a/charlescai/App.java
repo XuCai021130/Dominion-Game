d272bf67d83aa3083a58b53cee1ab7516600c731
XuCai021130 <xucai7140@gmail.com>
2025-02-26 05:07:34 +0000

    initialization of project

---
+++ automation/src/main/java/edu/brandeis/cosi103a/charlescai/App.java
@@ -0,0 +1,13 @@
+package edu.brandeis.cosi103a.charlescai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
